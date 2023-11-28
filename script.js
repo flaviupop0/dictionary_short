bc3001b63ab3c9112e45f44ce8cb2d647bef5e97
flaviupop0 <flaviupop2001@gmail.com>
2023-11-28 12:31:36 +0200

    fixed a few things

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let words = [];
 
 function addWord() {
-    const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
+    const wordInput = document.querySelector(".cutom-input");
     const wordValue = wordInput.value.trim().toLowerCase();
     words.push(wordValue);
 }
 
 function searchWord() {
-    const wordInput = document.querySelector(".cutom-input:nth-of-type(1)");
+    const wordInput = document.querySelector(".cutom-input");
     const searchValue = wordInput.value.trim().toLowerCase();
     if (words.includes(searchValue)) {
         alert("This word exist in your dictionary");
